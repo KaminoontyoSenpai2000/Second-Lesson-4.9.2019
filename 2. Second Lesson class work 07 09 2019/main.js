461ab98a8aba90a550773d7c35ff5a8dd760e7cf
Ivan <49728539+KaminoontyoSenpai2000@users.noreply.github.com>
2019-09-07 21:17:35 +0300

    Add files via upload

---
+++ 2. Second Lesson class work 07 09 2019/main.js
@@ -0,0 +1,24 @@
+var a;
+var a = 1;
+console.log(a);
+
+var q = 5;
+var w = q;
+console.log(w);
+
+var z = (q = w);
+console.log(z);
+
+var e = 20;
+var r = 6;
+var t = (e % r); /*% - это остаток от деления*/
+console.log(t);
+
+var p = 30;
+var o = o + 1.3;
+var o += 1.3;
+console.log(o); 
+
+/*ДОРАЗБЕРИСЬ С СОКРАЩЕНИЯМИ!!!!!!*/
+
+
